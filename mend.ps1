120f5705c6242fccf52bc810c4c9a6224b3cb3cf
Jorge Apodaca <mcsd@jorge-apodaca.net>
2025-01-09 21:11:35 -0800

    more changes

--- mend.ps1
+++ mend.ps1
@@ -4,7 +4,7 @@ param(
 	[ValidateSet("SCA", "SAST")]
 	[Parameter(Mandatory=$true)]
 	[string]$scanType,
-	[ValidateSet("BaseLine","Incremental")]
+	[ValidateSet("None","BaseLine","Incremental")]
 	[Parameter(Mandatory=$false)]
 	[string]$sastType="BaseLine"
 )
@@ -18,8 +18,11 @@ try {
 			if($sastType -eq "BaseLine") {
 				mend sast -d $directoryPath -s "POC_APP_MEND//POC_PROJ_API_01" --label-app "POC-App" --label-proj "POC-Proj" --upload-baseline --csharp-engine-generation 2
 			}
-			else {
+			elseif($sastType -eq "Incremental"){
 				mend sast -d $directoryPath -s "POC_APP_MEND//POC_PROJ_API_01" --label-app "POC-App" --label-proj "POC-Proj" --inc --csharp-engine-generation 2
+			}
+			elseif($scanType -eq "None") {
+				mend sast -d $directoryPath -s "POC_APP_MEND//POC_PROJ_API_01" --label-app "POC-App" --label-proj "POC-Proj" --csharp-engine-generation 2
 			}
 		}
 	}
